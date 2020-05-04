3c00027c0531ac993d47f47284d888b2181d9cac
achyuth <achyuth@localhost.localdomain>
2020-05-04 12:26:52 +0530

    checking amplify

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ function App() {
     <div className="App">
       <header className = "App-header">
         <h1> hello this is srikanth </h1>
-        <h1> founder of svtconstructions pvt ltd</h1>
-        <h1> i construct buildings </h1>
-        <h1> buildings take time </h1>
-        <h1> website under contructions</h1>
-        <h2> take diversion</h2>
-        <h1> call me : 9663940099</h1>
+        <img src = {logo} className="App-logo" />
+      
       </header>
       <h1> site built by an idiot</h1>
 
